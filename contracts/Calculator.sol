ddae8b769d19cc324c650909fa333b50963bf2a4
John Chang <johnchang9094@gmail.com>
2017-09-24 14:57:14 +0100

    Implementing Interface

---
+++ contracts/Calculator.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.0;
+
+contract Calculator {
+    uint addResult;
+    
+    function doAddition(uint num1, uint num2) returns (uint) {
+        addResult = num1 + num2;
+        return addResult;
+    }
+    
+    function calldoAddition() returns (uint) {
+        return doAddition(4,6);
+    }
+    
+    function getResult() constant returns (uint) {
+        return addResult;
+    }
+    
+}
+
+contract Interface {
+    uint public interResult;
+    
+    function addNumber(uint num3, uint num4, address calcAddress) returns (uint) {
+        Calculator calc = Calculator(calcAddress);
+        return calc.doAddition(num3, num4);
+    }
+}
