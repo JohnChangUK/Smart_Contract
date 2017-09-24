9babf0a7ca74bd2f20fdad7a3f0b9f582929f0f6
John Chang <johnchang9094@gmail.com>
2017-09-24 23:32:34 +0100

    Implementing more functions to Contract

--- contracts/Calculator.sol
+++ contracts/Calculator.sol
@@ -3,8 +3,37 @@ pragma solidity ^0.4.0;
 
 contract Calculator {
     uint public addResult;
+    address owner;
+    string word = "Hello World";
+    mapping (address => uint) balances;
     
-    function doAddition(uint num1, uint num2) returns (uint) {
+    function Calculator() {
+        owner = msg.sender;
+    }
+    
+    function getWord() constant returns (string) {
+        return word;
+    }
+    
+    function getBalance(address _addr) returns (uint) {
+        return balances[_addr];
+    }
+    
+    function increaseBalance(address _addr) returns (uint) {
+        balances[_addr] += 200;
+        return balances[_addr];
+
+    }
+
+    function setWord(string newWord) returns (string) {
+        if (owner != msg.sender) {
+            return "You are not the owner, access DENIED";
+        }
+        word = newWord;
+        return "You are the owner of the contract!";
+    }
+    
+    function doAddition(uint num1, uint num2) public returns (uint) {
         addResult = num1 + num2;
         return addResult;
     }
@@ -15,7 +44,7 @@ contract Calculator {
     
     function getResult() constant returns (uint) {
         return addResult;
-    }
+    } 
 }
 
 
@@ -29,7 +58,7 @@ contract Interface {
 }
 
 
-contract ChildCalculator is Calculator {
+contract SuperCalculator is Calculator {
     function getFinalResult() returns (uint) {
         return addResult;
     }
