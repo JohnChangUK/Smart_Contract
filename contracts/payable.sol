193d73535c04a20f860a85d286dabbfe0202ed4e
John Chang <johnchang9094@gmail.com>
2017-09-25 22:57:47 +0100

    Truffle init

--- contracts/payable.sol
+++ contracts/payable.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.4.0;
 contract TestTransfer {
     
     function TestTransfer() payable {
-        
+        return "Constructor with payable modifier"
     }
     
     function receiveFund() payable {
-        
+        return "receiveFund with payable modifier"
     }
     
     function getBalance(address _addr) returns (uint) {
