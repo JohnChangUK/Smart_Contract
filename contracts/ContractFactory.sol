e32b00f44eaa39922af0aa6449aede48d3a0488c
John Chang <johnchang9094@gmail.com>
2017-09-30 21:29:09 +0100

    Create Contract Factory Class

--- contracts/ContractFactory.sol
+++ contracts/ContractFactory.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.4;
 
 contract ChildContract {
 
-    uint public identity;
+    uint8 public identity;
     address public owner;
     bytes32 public name;
 
@@ -21,7 +21,7 @@ contract ChildContract {
         name = name;
     }
 
-    function transferOwnership(address newOwner, bytes name) OwnerOnly {
+    function transferOwnership(address newOwner, bytes32 name) OwnerOnly {
         bytes32 former = name;
         owner = newOwner;
         name = name;
@@ -29,11 +29,60 @@ contract ChildContract {
     }
 
     // Check if the Caller is the Owner
-    function isOwner(address addr) returns bool {
+    function isOwner(address addr) returns (bool) {
         return (addr == owner);
     }
 }
 
 contract ContractFactory {
     ChildContract[] children;
+
+    uint public initialPrice;
+
+    function ContractFactory(uint8 numParts, uint8 price) {
+        for(uint8 i = 0; i < numParts; i++) {
+            children.push(new ChildContract(i, this, "****"));
+        }
+        initialPrice = price;
+    }
+
+    // Anyone can pay the price and purchase the asset
+
+    function purchase(bytes32 name) payable {
+        // msg.value is the value received
+        if (msg.value < initialPrice)
+        revert();
+    // Looks for Available Asset, i.e One that is not sold yet
+    for (uint8 i = 0; i < children.length; i++) {
+        // Check if the contract factory is the owner
+        if (children[i].isOwner(this)) {
+            children[i].transferOwnership(msg.sender, name);
+            return;
+        }
+    }
+    // No more assets available - so throw an exception
+    revert();
+}
+
+// Returns the information about the child contract at specified index
+function getInfo(uint8 childIndex) constant returns (uint8, address, bytes32) {
+    return (children[childIndex].identity(), children[childIndex].owner(), children[childIndex].name());
+}
+
+// Returns the child contract address
+function getChildContractAddress(uint8 childIndex) returns (address) {
+    return address(children[childIndex]);
+}
+
+// Returns name of the owner based on the child index
+function getOwnerName(uint8 childIndex) constant returns (bytes32) {
+    bytes32 ownerName = children[childIndex].name();
+    return ownerName;
+}
+
+// Returns the count of the children
+function getChildrenCount() constant returns (uint) {
+    return children.length;
+}
+
 }
